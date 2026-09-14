42b92371b6a31c5673944fc4489e00e257ec941c
Adrian Bednorz <a.bednorz@wasko.pl>
2026-09-14 11:09:11 +0200

    Remove obj/ directory, add .gitignore

--- ScrabbleBackend/ScrabbleBackend/Program.cs
+++ ScrabbleBackend/ScrabbleBackend/Program.cs
@@ -10,6 +10,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "v1"); });
 }
 
 var summaries = new[]
